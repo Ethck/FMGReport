7526151948d720469eb34a5adcbdfc6c8ce29fcf
Ethck <dstrobe@uw.edu>
2019-09-02 16:51:33 -0700

    Refactored to PEP8, added scroll background

--- main.py
+++ main.py
@@ -2,117 +2,157 @@ from fpdf import FPDF
 from fpdf.html import hex2dec as h2d
 import pandas as pd
 
-#CONSTANTS
-pdf = FPDF()
-EPW = pdf.w - 2*pdf.l_margin
-BAD_LETTERS = ["g", "j", "y", "p"]
-
-#Active Variables
-stateStarts = {}
+nationStarts = {}
 pageNum = 0
+pdf = FPDF()
 
-#FUNCTIONS
-def makeStatePage(state, relations):
-	global pageNum, stateStarts
-
-	pdf.set_line_width(3)
-
-	pdf.set_font("Times", size=72)
-	pdf.add_page()
-
-	state_color = h2d(state["Color"])
-	pdf.cell(EPW, 60, state["State"], align="C")
-	pdf.set_draw_color(*state_color)
-
-	if any(badLetter in state["State"] for badLetter in BAD_LETTERS):
-		pdf.line(0,56,500,56)
-	else:
-		pdf.line(0,51,500,51)
-
-	pdf.set_xy(15, 52)
-	pdf.set_font("Times", size=36)
-	pdf.cell(30, 20, "Key Info")
+EPW = pdf.w - 2*pdf.l_margin
+DESCENDERS = ["g", "j", "y", "p"]
 
 
-	pdf.set_font("Times", size=18)
-	pdf.set_xy(10,70)
+def make_nation_page(nation, relations):
+    """Create the basic page for every nation.
 
-	frame_key_info_y = pdf.get_y()
-	pdf.cell(0, 10, f"Capital: {state['Capital']}", 0, 2)
-	pdf.cell(0, 10, f"Burgs: {state['Burgs']}", 0, 2)
-	pdf.cell(0, 10, f"Area (mi^2): {state['Area mi2']}", 0, 2)
-	pdf.cell(0, 10, f"Population: {state['Population']}", 0, 1)
-	frame_key_info_y2 = pdf.get_y()
+    This page contains key info as well as the nation's name and color.
+    Then, additional sections are added end of the function.
+    """
+    global pageNum, nationStarts
 
-	pdf.set_xy(8, 70)
-	pdf.cell(65, frame_key_info_y2 - frame_key_info_y , border = 1)
+    pdf.set_line_width(3)
 
-	stateStarts[state["State"]] = pdf.page_no()
-	pageNum = pdf.page_no()
+    pdf.set_font("Times", size=72)
+    pdf.add_page()
 
-	makeRelationSection(relations[state["State"]], state["State"])
-	makeProvincialSection(state["State"])
+    #Set background image
+    pdf.image("data/scroll_background.jpg", 0, 0)
 
-def makeRelationSection(relations, curr_state):
-	pdf.set_line_width(1)
-	pdf.set_xy(15, 120)
-	pdf.set_font("Times", size=36)
-	pdf.cell(30, 20, "Relations")
-	pdf.set_xy(8, 135)
-	pdf.set_font("Times", size=12)
+    #Write the nation's name in its color
+    nation_color = h2d(nation["Color"])
+    pdf.cell(EPW, 60, nation["State"], align="C")
+    pdf.set_draw_color(*nation_color)
 
-	global stateNames
-	for i, state in enumerate(stateNames):
-		if i >= 1:
-			pdf.set_x(8)
+    #DESCENDERS adjustment
+    title_y = 51
+    if any(descender in nation["State"] for descender in DESCENDERS):
+        title_y += 5
 
-		th = pdf.font_size + 1
+    pdf.line(0, title_y, 500, title_y)
 
-		if any((badLetter in state) or (badLetter in relations[i]) for badLetter in BAD_LETTERS):
-			th + 1
+    #Make Title
+    pdf.set_xy(15, 52)
+    pdf.set_font("Times", size=36)
+    pdf.cell(30, 20, "Key Info")
 
-		pdf.cell(EPW/4, th, f"{state}", border=1, ln= 0)
-		pdf.cell(EPW/10, th, f"{relations[i]}", border=1, ln=1)
+    #Return to normal
+    pdf.set_font("Times", size=18)
+    pdf.set_xy(10,70)
 
-def makeProvincialSection(curr_state):
-	selection = provincesDF.loc[provincesDF["State"] == curr_state]
-	pdf.set_xy(120, 55)
-	pdf.set_font("Times", size=36)
-	pdf.cell(30, 20, "Provinces")
+    #Print all key info
+    frame_key_info_y = pdf.get_y()
+    pdf.cell(0, 10, f"Capital: {nation['Capital']}", 0, 2)
+    pdf.cell(0, 10, f"Burgs: {nation['Burgs']}", 0, 2)
+    pdf.cell(0, 10, f"Area (mi^2): {nation['Area mi2']}", 0, 2)
+    pdf.cell(0, 10, f"Population: {nation['Population']}", 0, 1)
+    frame_key_info_y2 = pdf.get_y()
 
-	pdf.set_xy(100, 70)
-	pdf.set_font("Times", size=12)
-	for i in range(len(selection.index)):
-		th = pdf.font_size + 1
-		pdf.set_x(90)
-		row = selection.iloc[[i]].to_numpy()[0]
-		pdf.set_draw_color(*h2d(row[4]))
+    #Store it inside a box
+    pdf.set_xy(8, 70)
+    pdf.cell(65, frame_key_info_y2 - frame_key_info_y , border = 1)
 
-		if any((badLetter in row[2]) for badLetter in BAD_LETTERS):
-			th += 1
+    #Record where this page is, update total page count
+    nationStarts[nation["State"]] = pdf.page_no()
+    pageNum = pdf.page_no()
 
-		pdf.cell(EPW/4, th, f"{row[2]} of {row[1]}", 1, 0)
-		pdf.cell(EPW/8, th, f"Area: {row[6]}", 1, 0)
-		pdf.cell(EPW/4, th, f"Pop.: {row[7]}", 1, 2)
+    #Add additional sections
+    pdf.set_line_width(1)
+    make_relation_section(relations[nation["State"]], nation["State"])
+    make_provinces_section(nation["State"])
 
+
+def make_relation_section(relations, curr_nation):
+    """Add a relations section to the nation's page that details
+    relations with all other nations.
+    """
+
+    #Create the section header
+    pdf.set_xy(15, 120)
+    pdf.set_font("Times", size=36)
+    pdf.cell(30, 20, "Relations")
+
+    #Return to normal
+    pdf.set_xy(8, 135)
+    pdf.set_font("Times", size=12)
+    pdf.set_draw_color(0, 0, 0)
+
+    global nation_names
+    for i, nation in enumerate(nation_names):
+        if i >= 1:
+            pdf.set_x(8)
+
+        height = pdf.font_size + 1
+
+        #If letter is a descender, increase space allowed.
+        if any((descender in nation) or (descender in relations[i]) for
+                descender in DESCENDERS):
+            height += 1
+
+        #Target nation and their relation with curr_nation.
+        pdf.cell(EPW/4, height, f"{nation}", border=1, ln= 0)
+        pdf.cell(EPW/10, height, f"{relations[i]}", border=1, ln=1)
+
+
+def make_provinces_section(curr_nation):
+    """Add a provinces section to the nation's page to show all
+    active provinces within the nation.
+    """
+
+    #Select only the provinces that are inside of curr_nation
+    selection = provinces_DF.loc[provinces_DF["State"] == curr_nation]
+
+    #Make Header
+    pdf.set_xy(120, 55)
+    pdf.set_font("Times", size=36)
+    pdf.cell(30, 20, "Provinces")
+
+    #Return to normal
+    pdf.set_xy(100, 70)
+    pdf.set_font("Times", size=12)
+
+    for i in range(len(selection.index)):
+        #Grab row, convert to array, set borders to province color
+        th = pdf.font_size + 1
+        pdf.set_x(85)
+        row = selection.iloc[[i]].to_numpy()[0]
+        pdf.set_draw_color(*h2d(row[4]))
+
+        #Compensate for DESCENDERS
+        if any((descender in row[2]) for descender in DESCENDERS):
+            th += 1
+
+        #{form} of {name} | {area} | {population}
+        #EX: Barony of Elvelavo | 5.4k | 113k
+        pdf.cell(EPW/4, th, f"{row[2]} of {row[1]}", 1, 0)
+        pdf.cell(EPW/7, th, f"Area: {row[6]}", 1, 0)
+        pdf.cell(EPW/4, th, f"Pop.: {row[7]}", 1, 2)
 
 
 #MAIN
 pdf.set_font("Times", size=12)
 
-relationsDF = pd.read_csv("data/state_relations_data_test.csv")
-relationsDF = relationsDF.sort_values(by = "Target")
-stateNames = relationsDF['Target'].tolist()
+#Load relations, sort to alphabetical by origin nation
+relations_DF = pd.read_csv("data/state_relations_data_test.csv")
+relations_DF = relations_DF.sort_values(by = "Target")
+nation_names = relations_DF['Target'].tolist()
 
-provincesDF = pd.read_csv("data/provinces_data_test.csv")
+#Load provinces
+provinces_DF = pd.read_csv("data/provinces_data_test.csv")
 
-states = pd.read_csv("data/states_data_test.csv")
-states.drop(states.tail(1).index, inplace=True)
-states.apply(makeStatePage, axis=1, relations = relationsDF)
+#Load nations, drop the neutrals row, for each nation make a page
+nations = pd.read_csv("data/states_data_test.csv")
+nations.drop(nations.tail(1).index, inplace=True)
+nations.apply(make_nation_page, axis=1, relations = relations_DF)
 
-#relationsDF.apply(makeRelationSection, axis=1)
-print(stateNames)
-
-
+#Write to PDF, and say we are finished.
 pdf.output("demo.pdf")
-print(f"Finished making {pageNum} pages.")
+print(
+    f"Finished making {pageNum} pages about all {len(nation_names)} nations.")
